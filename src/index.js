0971ad61f1256f027ec304b0e237b7c11d709894
Vivekanand Rao <vivekanand.rao@gmail.com>
2021-07-20 16:05:59 -0700

    Dev Environment Build.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,8 @@
-/*eslint-disable import/default */
-import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
-import configureStore from './store/configureStore';
-import {Provider} from 'react-redux';
-import { Router, browserHistory } from 'react-router';
-import routes from './routes';
-import {loadCourses} from './actions/courseActions';
-import {loadAuthors} from './actions/authorActions';
-import './styles/styles.css';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import '../node_modules/toastr/build/toastr.min.css';
 
-const store = configureStore();
-store.dispatch(loadCourses());
-store.dispatch(loadAuthors());
+function HelloWorld() {
+  return <h1>Hello World</h1>;
+}
 
-render(
-  <Provider store={store}>
-    <Router history={browserHistory} routes={routes}/>
-  </Provider>,
-  document.getElementById('app')
-);
+render(<HelloWorld />, document.getElementById('app'));
